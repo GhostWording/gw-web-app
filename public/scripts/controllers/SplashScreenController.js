e87e34da24a578ed9d2f1891ff8b62a873538230
Rui Carvalho <github@rui.fr>
2014-01-22 17:36:00 +0100

    removed previously created root folder « public »

--- public/scripts/controllers/SplashScreenController.js
+++
@@ -1,9 +0,0 @@
-// Display general information about our App
-cherryApp.controller('SplashScreenController', ['$scope', 'HelperService','PostActionSvc',
-    function ($scope, HelperService,PostActionSvc) {
-
-        $scope.Tabs.showTabs = false;
-//        console.log('SplashScreenController');
-        $scope.PostBox = PostActionSvc;
-
-    }]);
