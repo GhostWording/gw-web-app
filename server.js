f11432db2cea2309c01d73d40856106b520549e0
Rui Carvalho <github@rui.fr>
2014-01-23 15:53:18 +0100

    server is now configured to serve the current directory as root, this will allow to simple serve development files in the root of the repo and the production files when inside the build folder

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ var app = express();
 var util = require('util');
 
 app.configure(function(){
-    app.use(express.static(__dirname + '/dist'));
+    app.use(express.static(__dirname));
 });
 
 app.get('/assets/*',function(req,res,next){
     util.puts('File: ' + req.url);
-    res.sendfile(__dirname + '/build' + req.url);
+    res.sendfile(__dirname + req.url);
 });
 
 app.get('/scripts/*',function(req,res,next){
     util.puts('File: ' + req.url);
-    res.sendfile(__dirname + '/build' + req.url);
+    res.sendfile(__dirname + req.url);
 });
 
 app.get('/views/*',function(req,res,next){
     util.puts('File: ' + req.url);
-    res.sendfile(__dirname + '/build' + req.url);
+    res.sendfile(__dirname  + req.url);
 });
 
 app.get("/*", function(req, res, next){
     util.puts('Path' + req.url);
 
-    res.sendfile(__dirname + '/build/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
 app.listen(3000);
