79544e074af48ee7c4d007d6718f71702d6f7789
Peter Bacon Darwin <pete@bacondarwin.com>
2014-02-05 11:16:23 +0000

    feat(cachSvc): add new service that will hold cached data

---
+++ scripts/services/cacheSvc.js
@@ -0,0 +1,61 @@
+// A simple wrapper around window.localStorage
+cherryApp.factory('localStorage', function($window) {
+  return {
+    get: function(key) {
+      return $window.localStorage.getItem(key);
+    },
+    set: function(key, value) {
+      if ( value ) {
+        return $window.localStorage.setItem(key, value);
+      } else {
+        removeItem(key);
+      }
+    }
+  };
+});
+
+// A cache that stores in-memory, then localStorage then calls externally
+cherryApp.factory('cacheSvc', function(localStorage, $q) {
+  var cache = {};
+  return {
+    _cache: cache,
+    register: function(name, getFn) {
+      cache[name] = {
+        name: name,
+        getFn: getFn
+      };
+    },
+
+    update: function(name, lastChange) {
+      if ( !cache.lastChange || cache.lastChange < lastChange ) {
+        delete cache.promise;
+        localStorage.set(cache.key, null);
+        cache.lastChange = lastChange;
+      }
+    },
+
+    get: function(name) {
+      var cacheEntry = cache[name];
+      if ( !cacheEntry.promise ) {
+        // We don't have a promise for the data
+        
+        // First try local storage
+        var localValue = localStorage.get(name);
+        if ( localValue ) {
+          cacheEntry.promise = $q.when(localValue);
+        }
+
+        // Not in local storage so use the registered function to get it
+        else {
+          cacheEntry.promise = $q.when(cacheEntry.getFn());
+
+          // Then when the data arrives add it to localStorage
+          cacheEntry.promise.then(function(value) {
+            localStorage.set(name, value);
+          });
+        }
+      }
+      return cacheEntry.promise;
+    }
+  };
+});
