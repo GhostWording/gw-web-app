608fc95820bd1bd3e364263c0751718bf6a3028e
Peter Bacon Darwin <pete@bacondarwin.com>
2014-02-07 14:41:20 +0000

    style(intentionApi): fix typo

--- scripts/services/api/intentionApi.js
+++ scripts/services/api/intentionApi.js
@@ -4,14 +4,14 @@ cherryApp.factory('intentionApi', function($http, apiUrl, currentLanguage) {
     forArea: function(areaName) {
       return $http.get(apiUrl +  areaName +  '/intentions',
           { headers: { 'Accept-Language': currentLanguage.code } }).then(function(response) {
-        console.log('Request intensions for area "' + areaName + '"; responded with ' + response.status);
+        console.log('Request intentions for area "' + areaName + '"; responded with ' + response.status);
         return response.data;
       });
     },
     one: function(areaName, intentionId) {
       return $http.get(apiUrl + areaName + '/intention/' + intentionId,
         { headers: { 'Accept-Language': currentLanguage.code } }).then(function(response) {
-        console.log('Request intension "' + intentionId + '" for area "' + areaName + '"; responded with ' + response.status);
+        console.log('Request intention "' + intentionId + '" for area "' + areaName + '"; responded with ' + response.status);
         return response.data;
       });
     }
