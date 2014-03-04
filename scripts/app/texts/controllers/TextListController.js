7a94a43d2eb4d0999c475a2ee51815d6c87ba97a
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-04 12:48:56 +0000

    refact(TextListController): move filtering to filterSvc

--- scripts/app/texts/controllers/TextListController.js
+++ scripts/app/texts/controllers/TextListController.js
@@ -2,18 +2,33 @@ angular.module('app/texts/TextListController', [])
 
 // Displays a list of texts
 .controller('TextListController',
- ['$scope', 'currentTextList', 'currentIntention', 'currentArea', 'filtersSvc',
-function ($scope, currentTextList, currentIntention, currentArea, filtersSvc) {
+ ['$scope', 'currentTextList', 'currentIntention', 'currentArea', 'currentUser', 'filtersSvc',
+function ($scope, currentTextList, currentIntention, currentArea, currentUser, filtersSvc) {
 
     $scope.currentArea = currentArea;
     $scope.currentIntention = currentIntention;
     $scope.textList = currentTextList;
+    $scope.filteredList = [];
 
     $scope.filters = filtersSvc.filters;
     $scope.filtersWellDefined = filtersSvc.wellDefined;
 
-    $scope.$watch(function() { return filtersSvc.filters; }, function updateFilteredList() {
-      $scope.filteredList = filtersSvc.filterList($scope.textList);
-    });
+
+    $scope.filterList = function() {
+      
+      // Clear the previous filter list
+      $scope.filteredList.length = 0;
+
+      // Add back in texts that are compatible with the current filters
+      angular.forEach($scope.textList, function(text) {
+        if ( filtersSvc.textCompatible(text, currentUser) ) {
+          $scope.filteredList.push(text);
+        }
+      });
+
+    };
+
+    // Watch the filters and update the filtered text list if they change
+    $scope.$watch(function() { return filtersSvc.filters; }, $scope.filterList, true);
 
 }]);
