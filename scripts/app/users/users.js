18e8d944bbd0089790601ec75c429137ab893689
OlivierBourdin <olivier.bourdin@gmail.com>
2014-03-10 15:49:11 +0000

    feat(users): send subscription email to server for validation

--- scripts/app/users/users.js
+++ scripts/app/users/users.js
@@ -26,12 +26,50 @@ angular.module('app/users', [])
   return currentUser;
 
 }])
+// We may not want to send this data to the server with each request (email might not be a good idea and subscription might be to heavy)
+// So in the end we might use local storage for all user properties
+.factory('currentUserLocalData',['$rootScope','localStorage','deviceIdSvc',function($rootScope,localStorage,deviceIdSvc) {
+
+	var key = 'currentUserLocalData' + '.' + deviceIdSvc.get();
+
+	var currentUserLocalData = localStorage.get(key) || {
+		email: null,
+		subcriptions: null
+	};
+
+	$rootScope.$watch(function() { return currentUserLocalData; }, function(value, oldValue) {
+		if ( value !== oldValue ) {
+			localStorage.set(key, currentUserLocalData);
+		}
+	}, true);
+
+	return currentUserLocalData;
+}])
 
 .controller('UserProfileController', ['$scope', 'currentUser', 'userAges', function ($scope, currentUser, userAges) {
   $scope.currentUser = currentUser;
   $scope.userAges = userAges;
 }])
 
-.controller('UserEMailController', ['$scope', 'serverSvc',  function ($scope, serverSvc) {
-		serverSvc.postInStore('testStore','testKey','testValue');
+.controller('UserEMailController', ['$scope', 'serverSvc','deviceIdSvc','currentUserLocalData',  function ($scope, serverSvc,deviceIdSvc,currentUserLocalData) {
+	console.log(deviceIdSvc.get());
+	$scope.user = currentUserLocalData;
+	$scope.mailChanged = false;
+	$scope.mailSent = false;
+
+	$scope.updateMail = function() {
+		$scope.mailChanged = true;
+		$scope.mailSent = false;
+	};
+
+	$scope.sendMailToServer = function () {
+		currentUserLocalData.email = $scope.user.email;
+		if ($scope.mailChanged) {
+			serverSvc.postInStore('mailStore', deviceIdSvc.get(), $scope.user.email)
+			.then(function (response) {
+				$scope.mailSent = true;
+				$scope.mailChanged = false;
+			});
+		}
+	};
 }]);
