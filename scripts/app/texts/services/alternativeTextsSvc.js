da18ea695ee35e230580de0757ad7f052a68623c
OlivierBourdin <olivier.bourdin@gmail.com>
2014-05-12 15:31:00 +0100

    fix(alternativeTexts): minor bugs

--- scripts/app/texts/services/alternativeTextsSvc.js
+++ scripts/app/texts/services/alternativeTextsSvc.js
@@ -33,16 +33,16 @@ angular.module('app/texts/alternativeTextList', [])
 .factory('alternativeTextsSvc', ['cacheSvc', 'serverSvc','HelperSvc','currentLanguage','CultureCollection','availableLanguages',
   function( cacheSvc, serverSvc,HelperSvc,currentLanguage,CultureCollection,availableLanguages) {
     var service = {
-      // This function gets other realisations (=equivalent texts), for alternative languages, polite forms, sender, or recipient
-      getRealizationList: function(areaName,textId) {
+      // Get alternative realisations (=equivalent texts given a prototype id), for alternative languages, polite forms, sender, or recipient
+      getRealizationList: function(areaName,textPrototypeId) {
         // http://api.cvd.io/        GET /{areaName}/text/realizations/{realizationId}
-        var path = areaName + '/text/realizations/' + textId;
-        console.log("getRealizationList called for area : " + areaName + ", textId : " + textId);
-        console.log("path : " +path);
+        var path = areaName + '/text/realizations/' + textPrototypeId;
+//        console.log("getRealizationList called for area : " + areaName + ", ProtytypeId : " + textPrototypeId);
+//        console.log("path : " +path);
         // TODO : think about caching when we have a cache invalidation policy
         return serverSvc.get(path,null,null,'fr-FR');
       },
-      // Returns a collection of available cultures;
+      // cultures that appear at least once in a text list;
       getCultures: function(textList) {
         var availableCultures = new CultureCollection();
         for ( var i = 0; i < textList.length; i++) {
@@ -54,37 +54,43 @@ angular.module('app/texts/alternativeTextList', [])
         }
         return availableCultures;
       },
-      getTextsForLanguage: function(textList,targetCode) {
+      // selects texts in a givent language from a text list
+      getTextsForLanguage: function(textList,languageCodeForTextGroup) {
         var texts = [];
         for ( var i = 0; i < textList.length; i++) {
           var text = textList[i];
-          var languageCode = currentLanguage.getLanguageFromCulture(text.Culture);
-          if ( languageCode == targetCode) {
+          var textLanguageCode = currentLanguage.getLanguageFromCulture(text.Culture);
+          //if ( languageCode == targetCode) { // TODO : put this back
+          if ( textLanguageCode == languageCodeForTextGroup ||  ( textLanguageCode == 'sp' && languageCodeForTextGroup == 'es'  ) ) {   // HACK around server bug
             texts.push(text);
           }
         }
         return texts;
       },
+      findBestMatches: function(currentText,textList) {
+        return textList;
+      },
+      // creates an array where each entry points to a text list in a language
       getAlternativeTexts: function(text, textList) {
         console.log("Nb alternative realizations : " + textList.length);
-        var availableCultures = service.getCultures(textList);
+        //var availableCultures = service.getCultures(textList);
         var currentLanguageCode = currentLanguage.getLanguageCode();
-        var orderedPresentationLanguages =  availableLanguages.orderedAppLanguages(currentLanguageCode);
+        var applicationLanguages =  availableLanguages.orderedAppLanguages(currentLanguageCode);
         //console.log(availableCultures);
-        console.log(orderedPresentationLanguages);
+        //console.log(applicationLanguages);
         // For each alternative language, get gather texts related to the same prototype
         var excludeCurrentLanguage = true;
         var textArraysForLanguages = [];
-        for (var i = 0; i < orderedPresentationLanguages.length; i++ ) {
-          var code = orderedPresentationLanguages[i];
-          var texts =  service.getTextsForLanguage(textList,code);
-          console.log(texts.length + " texts for " + code);
-          if ( excludeCurrentLanguage && code == currentLanguageCode )
+        for (var i = 0; i < applicationLanguages.length; i++ ) {
+          var languageCode = applicationLanguages[i];
+          var texts =  service.getTextsForLanguage(textList,languageCode);
+          console.log(texts.length + " texts for " + languageCode);
+          if ( excludeCurrentLanguage && languageCode == currentLanguageCode )
             continue;
           if ( texts.length > 0) {
             // TODO : if several texts, chose try to choose the best one
-            //var filteredList = findBestMatches(currentText,textList);
-            textArraysForLanguages.push( { "code": code, "texts" : texts    } );
+            var filteredList = service.findBestMatches(currentText,textList);
+            textArraysForLanguages.push( { "code": languageCode, "texts" : texts    } );
           }
         }
         // TODO : for each orderedPresentationLanguages, prepare an array of available texts for the language, then chose the best one according to sender, recipient and polite form
