aa48ec01ddbf94bb278fa4e3d1fdf81c5bcf19ce
OlivierBourdin <olivier.bourdin@gmail.com>
2014-07-24 18:54:52 +0100

    feat(splashscreen) : new layout

--- scripts/app/splash/SplashScreenController.js
+++ scripts/app/splash/SplashScreenController.js
@@ -1,5 +1,7 @@
 // Display general information about our App
 angular.module('app/splash', ['common/services'])
 
-.controller('SplashScreenController', ['$scope', function ($scope) {
+.controller('SplashScreenController', ['$scope','currentLanguage', function ($scope,currentLanguage) {
+
+  $scope.isFrench = currentLanguage.isFrenchVersion();
 }]);
