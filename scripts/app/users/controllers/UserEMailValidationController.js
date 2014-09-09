60fddccd678a41f1fa21e883bea4737a384c3f31
OlivierBourdin <olivier.bourdin@gmail.com>
2014-09-09 11:24:21 +0100

    feat(*) : translation and tracking pixel

--- scripts/app/users/controllers/UserEMailValidationController.js
+++ scripts/app/users/controllers/UserEMailValidationController.js
@@ -3,4 +3,5 @@ angular.module('app/users/UserEMailValidationController', [])
 .controller('UserEMailValidationController', ['$scope', 'currentUserLocalData',  function ($scope, currentUserLocalData) {
   $scope.user = currentUserLocalData;
 
+
 }]);
