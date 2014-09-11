8725feaf38808180dcbca443349ced987792dfe3
OlivierBourdin <olivier.bourdin@gmail.com>
2014-09-11 11:01:51 +0100

    feat(userEmail) : facebook email registration

--- scripts/app/users/controllers/UserEMailController.js
+++ scripts/app/users/controllers/UserEMailController.js
@@ -1,10 +1,25 @@
 angular.module('app/users/UserEMailController', [])
 
-.controller('UserEMailController', ['$scope', 'serverSvc','deviceIdSvc','currentUserLocalData','currentLanguage','postActionSvc',  function ($scope, serverSvc,deviceIdSvc,currentUserLocalData,currentLanguage,postActionSvc) {
+.controller('UserEMailController', ['$scope', 'serverSvc','deviceIdSvc','currentUserLocalData','currentLanguage','postActionSvc','facebookSvc',  function ($scope, serverSvc,deviceIdSvc,currentUserLocalData,currentLanguage,postActionSvc,facebookSvc) {
   console.log(deviceIdSvc.get());
   $scope.user = currentUserLocalData;
   $scope.mailChanged = false;
   $scope.mailSent = false;
+
+  // Login
+  $scope.fbLogin = facebookSvc.fbLogin;
+
+  $scope.$watch(function() { return facebookSvc.isConnected();},function() {
+    $scope.isConnected = facebookSvc.isConnected();
+  },true);
+
+  $scope.$watch(function() { return facebookSvc.getCurrentMe();},function() {
+    console.log("facebookSvc.getCurrentMe() : " +facebookSvc.getCurrentMe());
+    $scope.apiMe = facebookSvc.getCurrentMe();
+    console.log("$scope.user.email : "  + $scope.user.email);
+    $scope.sendMailToServer();
+  },true);
+
 
   postActionSvc.postActionInfo('Init','Page','UserEmail','Init');
 
@@ -15,6 +30,14 @@ angular.module('app/users/UserEMailController', [])
   };
 
   $scope.sendMailToServer = function () {
+
+    console.log("$scope.user.email : "  + $scope.user.email);
+    console.log("currentUserLocalData.email : "  + currentUserLocalData.email);
+
+    if ( ! $scope.user.email ) {
+      console.log("$scope.user.email not set");
+      return;
+    }
 
     // Post new email to server
     serverSvc.postInStore('mailStore', deviceIdSvc.get(), $scope.user.email).then(function (response) {
