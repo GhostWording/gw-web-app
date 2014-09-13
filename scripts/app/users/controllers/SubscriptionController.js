fc9885b8844e6bf89a8d40b441782ffcf63a56ab
OlivierBourdin <olivier.bourdin@gmail.com>
2014-09-13 16:39:46 +0100

    feat(subscription) : yet one more cat

--- scripts/app/users/controllers/SubscriptionController.js
+++ scripts/app/users/controllers/SubscriptionController.js
@@ -3,6 +3,12 @@ angular.module('app/users/SubscriptionController',['app/recipients'])
 .controller('SubscriptionController', ['$scope', 'subscribedRecipientTypesSvc', 'subscriptionsSvc','serverSvc','currentUserLocalData','deviceIdSvc','recipientTypeHelperSvc','currentUser',
   function ($scope, subscribedRecipientTypesSvc, subscriptionsSvc,serverSvc,currentUserLocalData,deviceIdSvc,recipientTypeHelperSvc,currentUser) {
 
+    $scope.userHasModifiedSubscription = false;
+
+    $scope.subscriptionModified = function() {
+      $scope.userHasModifiedSubscription = true;
+    };
+
     subscriptionsSvc.getRecipientsWithSubscriptions().then(function (value) {
       //var compatibleRecipients = recipientTypeHelperSvc.getCompatibleRecipients(value,currentUser);
       $scope.recipientsWithSubscriptions = value;
