16ccad9164a126b05ecb86ec8ba080373853b6e8
OlivierBourdin <olivier.bourdin@gmail.com>
2014-10-03 13:36:00 +0100

    feat(userProfile) : add email and gender picture

--- scripts/app/users/controllers/UserProfileController.js
+++ scripts/app/users/controllers/UserProfileController.js
@@ -1,8 +1,10 @@
 angular.module('app/users/UserProfileController', [])
 
-.controller('UserProfileController', ['$scope', 'currentUser', 'userAges', 'localStorage','deviceIdSvc', function ($scope, currentUser, userAges,localStorage,deviceIdSvc) {
+.controller('UserProfileController', ['$scope', 'currentUser', 'currentUserLocalData', 'userAges', 'localStorage','deviceIdSvc','serverSvc','currentLanguage',
+function ($scope, currentUser,currentUserLocalData, userAges,localStorage,deviceIdSvc,serverSvc,currentLanguage) {
   $scope.currentUser = currentUser;
   $scope.userAges = userAges;
+  $scope.user = currentUserLocalData;
 
   $scope.clearAll = function() {
     // We want to delete everything but the deviceId
@@ -13,5 +15,23 @@ angular.module('app/users/UserProfileController', [])
     currentUser.age = null;
     currentUser.isDemonstrative = null;
     currentUser.clear();
+  };
+
+
+  $scope.sendMailToServer = function () {
+
+    // Should move that in a service and share with userEmail
+    serverSvc.postInStore('mailStore', deviceIdSvc.get(), $scope.user.email)
+    .then(function (response) {
+    })
+      // Ask server to send verification email
+    .then(function () {
+      serverSvc.postMailForVerification($scope.user.email);
+    })
+      // Send preferred culture to server
+    .then(function () {
+      serverSvc.postInStore('preferredCulture', deviceIdSvc.get(), currentLanguage.getCultureCode());
+    });
+
   };
 }]);
