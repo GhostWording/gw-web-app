05763d9391307b7fe0c606fc2a5ae788299fbd36
Sam Kuhn <dev@samkuhn.net>
2014-10-06 14:59:21 +0100

    feat(server): add colour to started log for better visibility

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
 var express = require("express");
 var app = express();
 var util = require('util');
+var colors = require('colors');
 
-app.configure(function(){
-    app.use(express.static(__dirname));
+app.configure(function() {
+	app.use(express.static(__dirname));
 });
 
-app.get('/assets/*',function(req,res,next){
-    util.puts('File: ' + req.url);
-    res.sendfile(__dirname + req.url);
+app.get('/assets/*',function(req, res, next) {
+	util.puts('file: ' + req.url);
+	res.sendfile(__dirname + req.url);
 });
 
-app.get('/scripts/*',function(req,res,next){
-    util.puts('File: ' + req.url);
-    res.sendfile(__dirname + req.url);
+app.get('/scripts/*',function(req, res, next) {
+	util.puts('file: ' + req.url);
+	res.sendfile(__dirname + req.url);
 });
 
-app.get('/views/*',function(req,res,next){
-    util.puts('File: ' + req.url);
-    res.sendfile(__dirname  + req.url);
+app.get('/views/*',function(req, res, next) {
+	util.puts('file: ' + req.url);
+	res.sendfile(__dirname  + req.url);
 });
 
-app.get("/*", function(req, res, next){
-    util.puts('Path' + req.url);
+app.get('/bower_components/*',function(req, res, next) {
+	util.puts('file: ' + req.url);
+	res.sendfile(__dirname  + req.url);
+});
 
-    res.sendfile(__dirname + '/index.html');
+app.get("/*", function(req, res, next) {
+	util.puts('path: ' + req.url);
+	res.sendfile(__dirname + '/index.html');
 });
 
 app.listen(3000);
 
-util.puts('server started on port 3000');
+util.puts('server started on port 3000'.blue);
