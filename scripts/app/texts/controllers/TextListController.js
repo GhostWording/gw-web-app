0e7cfbd61b05432383edfcbf5757ac9b1a95e6c0
OlivierBourdin <olivier.bourdin@gmail.com>
2014-10-13 16:19:26 +0100

    refac(TextListController): remove a couple of scope variables

--- scripts/app/texts/controllers/TextListController.js
+++ scripts/app/texts/controllers/TextListController.js
@@ -1,96 +1,83 @@
 angular.module('app/texts/TextListController', [])
 // Displays a list of texts
 .controller('TextListController',
- ['$scope', 'currentTextList', 'currentIntention', 'currentUser', 'filtersSvc', '$modal', 'currentRecipient', 'favouritesSvc','appUrlSvc','currentLanguage','textsSvc','intentionsSvc','currentAreaName','postActionSvc','$window','filteredTextListSvc','tagLabelsSvc','helperSvc','questionBarSvc','accordionSvc','$stateChange',
-function ($scope, currentTextList, currentIntention,  currentUser, filtersSvc, $modal,currentRecipient, favouritesSvc,appUrlSvc,currentLanguage,textsSvc,intentionsSvc,currentAreaName,postActionSvc,$window,filteredTextListSvc,tagLabelsSvc,helperSvc,questionBarSvc,accordionSvc,$stateChange) {
+ ['$scope', 'currentTextList', 'currentAreaName', 'currentIntention', 'currentUser', 'currentRecipient', 'currentLanguage', 'filtersSvc', 'favouritesSvc','appUrlSvc','textsSvc','intentionsSvc','postActionSvc','filteredTextListSvc','tagLabelsSvc','helperSvc','questionBarSvc','accordionSvc','$stateChange','$modal','$window',
+function ($scope, currentTextList, currentAreaName, currentIntention,  currentUser, currentRecipient, currentLanguage, filtersSvc, favouritesSvc,appUrlSvc,textsSvc,intentionsSvc,postActionSvc,filteredTextListSvc,tagLabelsSvc,helperSvc,questionBarSvc,accordionSvc,$stateChange,$modal,$window) {
+
   // We want an Init event even if no action takes place, in case user lands here from Google or facebook
   postActionSvc.postActionInfo('intention',currentIntention.IntentionId,'IntentionList','Init');
   // Some phone browser do not initialise the view correctly with  $location.hash('leCorps') and url does not look nice with   $anchorScroll()
   $window.scrollTo(0,0);
 
-  // Used by view to build urls
-  $scope.currentAreaName = currentAreaName;
-  $scope.currentIntention = currentIntention;
-  $scope.recipientId = $scope.currentRecipient ? $scope.currentRecipient.Id : 'none';
+  // Initial currentTextList might come from the cache. Check server for stale cache. If stale, require a new text list : will update content showed to user
+  intentionsSvc.invalidateCacheIfNewerServerVersionExists(currentAreaName,(!!currentIntention ) ? currentIntention.Slug : intentionsSvc.getCurrentId())
+  .then(function(shouldReload){if (shouldReload)getAndFilterTextList( currentLanguage.currentCulture() );});
 
-  // Give visibility to services
-  $scope.appUrlSvc = appUrlSvc;
-  $scope.helperSvc = helperSvc;
-  $scope.questionBarSvc = questionBarSvc;
-  $scope.filteredTextListSvc = filteredTextListSvc;
-  $scope.filters = filtersSvc.filters;
-  $scope.filtersWellDefined = filtersSvc.wellDefined;
-  $scope.currentRecipient = currentRecipient;
-  $scope.currentRecipientLabel = $scope.currentRecipient ?  $scope.currentRecipient.LocalLabel :  "";
-  $scope.theAccordionStatus = accordionSvc.theAccordionStatus;
-  $scope.openAccordion = function() {
-    $scope.theAccordionStatus.open = true;
-  };
-
-  // Read current text id from url : will switch display between text list and text detail view
+  // Read current text id from url (view will switch display from text list to text detail if textId is defined)
   $scope.getCurrentTextId = function() {
     var valret = $stateChange.toParams.textId;
     textsSvc.setCurrentTextId(valret);
     return valret;
   };
 
-  // Unfiltered text list : initialize with resolved text list from router
+  // Used by view to build urls, resolved from url
+  $scope.currentAreaName  = currentAreaName;
+  $scope.currentIntention = currentIntention;
+  // Recipient currently resolved from url
+  $scope.currentRecipient = currentRecipient;
+  $scope.recipientId      = $scope.currentRecipient ? $scope.currentRecipient.Id : 'none';
+  $scope.currentRecipientLabel = $scope.currentRecipient ?  $scope.currentRecipient.LocalLabel :  "";
+
+  // Give visibility to services
+  $scope.theHelperSvc = helperSvc;
+  $scope.theQuestionBarSvc = questionBarSvc;
+  $scope.filtersWellDefined = filtersSvc.wellDefined;
+  $scope.theAccordionStatus = accordionSvc.theAccordionStatus;
+  $scope.openAccordion = function() {
+    $scope.theAccordionStatus.open = true;
+  };
+
+  // Update filters with current recipient type
+  if ( currentRecipient )
+    filtersSvc.setRecipientTypeTag(currentRecipient.RecipientTypeTag);
+
+  // Unfiltered text list : initialize with resolved text list from routing
   var unfilteredTextList  = currentTextList;
-  // The filtered text list actualy displayed
+
+  // Filtered text list that is actualy displayed
   $scope.filteredList = [];
 
-  // Prepare the filtered text list to be displayed. On initialisation, do it with resolved text list
+  // Prepare the filtered text list to be displayed.
   var firstWatchCall = true;
   var filterList = function (textListToFilter) {
-    // Optimization : setFilteredAndOrderedList should not be called two times when view initializes. So we dont call it the first time
+    // filterList is called at least two times when view initializes. We skip the first time :-o
     if ( !firstWatchCall )
       filteredTextListSvc.setFilteredAndOrderedList(textListToFilter, currentUser, filtersSvc.filters.preferredStyles);
     firstWatchCall = false;
-    $scope.filteredList = filteredTextListSvc.getFilteredTextList();
-    return $scope.filteredList;
+    return filteredTextListSvc.getFilteredTextList();
   };
 
-  // Ask for new text list, count properties displayable to user, then filter list
-  var prepareAndDisplayTextList = function(culture) {
+  // Get text list for area, intention and culture, then filter it
+  var getAndFilterTextList = function(culture) {
     textsSvc.getCurrentTextList(culture).then(function(textList) {
-      unfilteredTextList =textList;
-      //textsSvc.countTextsForStylesAndProperties(textList);
-      accordionSvc.calculateMostSelectiveStyles();
-      filterList(unfilteredTextList);}
+      unfilteredTextList = textList;
+      $scope.filteredList = filterList(unfilteredTextList);}
     );
   };
 
-  // Get new list and filter when language changes
-  $scope.$watch(function() { return currentLanguage.getLanguageCode(); },prepareAndDisplayTextList( currentLanguage.currentCulture() ),true);
-  // Filter when user gender changes
-  $scope.$watch(function() { return currentUser.gender; },
-      function() {filterList(unfilteredTextList);},
-  true);
+  // Get and filter list when language changes
+  $scope.$watch(function() { return currentLanguage.getLanguageCode(); },getAndFilterTextList( currentLanguage.currentCulture() ),true);
+  // Filter list when user gender changes
+  $scope.$watch(function() { return currentUser.gender; },function() {$scope.filteredList = filterList(unfilteredTextList);},true);
   // Filter when filters change
-  $scope.$watch(function() { return filtersSvc.filters; },
-      function() {filterList(unfilteredTextList);},
-  true);
-  // Get new list and filter if server cache says it is stale
-  var intentionId = (!! currentIntention ) ? currentIntention.Slug : intentionsSvc.getCurrentId();
-  intentionsSvc.invalidateCacheIfNewerServerVersionExists(currentAreaName,intentionId)
-    .then(function(shouldReload){
-        if (shouldReload)
-          prepareAndDisplayTextList( currentLanguage.currentCulture() );
-        });
-
-  // Update filters with current recipient type : should be watched if it can change (currently does not change, it is read from the view url)
-  if ( currentRecipient ) {
-    filtersSvc.setRecipientTypeTag(currentRecipient.RecipientTypeTag); // Shoud not be reinitialized when we come back from TextDetail view
-  }
-
+  $scope.$watch(function() { return filtersSvc.filters; },function() {$scope.filteredList = filterList(unfilteredTextList);},true);
+  // Update styles suggested by accordion when unfiltered list changes
+  $scope.$watch(function() { return unfilteredTextList.length; }, function() {accordionSvc.calculateMostSelectiveStyles(); },true);
 
   // Get displayable style names for the text (imaginative, poetic,..)
   $scope.labelsThatShouldBeDisplayed = function(txt) {
-    var stylesWeWant = filtersSvc.filters.preferredStyles;
-    var idsWeWant = stylesWeWant.filterIds(txt.TagIds);
-    return tagLabelsSvc.labelsFromStyleTagIds(idsWeWant);
+    return tagLabelsSvc.labelsFromStyleTags(txt.TagIds,filtersSvc.filters.preferredStyles );
   };
-
 
   // Open send text dialog
   $scope.send = function(text) {
@@ -104,6 +91,5 @@ function ($scope, currentTextList, currentIntention,  currentUser, filtersSvc, $
       }
     });
   };
-
 
  }]);
