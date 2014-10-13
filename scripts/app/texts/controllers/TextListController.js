954fb8e240de03bd0beae65c4f16efbe14845e73
OlivierBourdin <olivier.bourdin@gmail.com>
2014-10-13 16:42:45 +0100

    refact(TextListController): a few more comments

--- scripts/app/texts/controllers/TextListController.js
+++ scripts/app/texts/controllers/TextListController.js
@@ -1,34 +1,35 @@
 angular.module('app/texts/TextListController', [])
 // Displays a list of texts
 .controller('TextListController',
- ['$scope', 'currentTextList', 'currentAreaName', 'currentIntention', 'currentUser', 'currentRecipient', 'currentLanguage', 'filtersSvc', 'favouritesSvc','appUrlSvc','textsSvc','intentionsSvc','postActionSvc','filteredTextListSvc','tagLabelsSvc','helperSvc','questionBarSvc','accordionSvc','$stateChange','$modal','$window',
-function ($scope, currentTextList, currentAreaName, currentIntention,  currentUser, currentRecipient, currentLanguage, filtersSvc, favouritesSvc,appUrlSvc,textsSvc,intentionsSvc,postActionSvc,filteredTextListSvc,tagLabelsSvc,helperSvc,questionBarSvc,accordionSvc,$stateChange,$modal,$window) {
+ ['$scope', 'currentTextList', 'currentAreaName', 'currentIntention', 'currentUser', 'currentRecipient', 'currentLanguage', 'filtersSvc', 'textsSvc','intentionsSvc','postActionSvc','filteredTextListSvc','tagLabelsSvc','helperSvc','questionBarSvc','accordionSvc','$stateChange','$modal','$window',
+function ($scope, currentTextList, currentAreaName, currentIntention,  currentUser, currentRecipient, currentLanguage, filtersSvc, textsSvc,intentionsSvc,postActionSvc,filteredTextListSvc,tagLabelsSvc,helperSvc,questionBarSvc,accordionSvc,$stateChange,$modal,$window) {
 
-  // We want an Init event even if no action takes place, in case user lands here from Google or facebook
+  // currentTextList might initialy come from the cache. Check server for stale cache. If stale, require a new text list : will update content showed to user
+  intentionsSvc.invalidateCacheIfNewerServerVersionExists(currentAreaName,(!!currentIntention ) ? currentIntention.Slug : intentionsSvc.getCurrentId())
+  .then(function(shouldReload){if (shouldReload)getAndFilterTextList( currentLanguage.currentCulture() );});
+
+  // We want an Init event even if no action takes place, in case user lands here from Google or facebook link
   postActionSvc.postActionInfo('intention',currentIntention.IntentionId,'IntentionList','Init');
   // Some phone browser do not initialise the view correctly with  $location.hash('leCorps') and url does not look nice with   $anchorScroll()
   $window.scrollTo(0,0);
 
-  // Initial currentTextList might come from the cache. Check server for stale cache. If stale, require a new text list : will update content showed to user
-  intentionsSvc.invalidateCacheIfNewerServerVersionExists(currentAreaName,(!!currentIntention ) ? currentIntention.Slug : intentionsSvc.getCurrentId())
-  .then(function(shouldReload){if (shouldReload)getAndFilterTextList( currentLanguage.currentCulture() );});
-
-  // Read current text id from url (view will switch display from text list to text detail if textId is defined)
+  // Read current text id from url (did not work well from routing.js)
   $scope.getCurrentTextId = function() {
+    // Text id will be undefined when we come back from text detail to text list
+    // That will tell the view to switch display from text detail to text list (could not figure how to do that in routing.js when comming back from text detail state)
     var valret = $stateChange.toParams.textId;
     textsSvc.setCurrentTextId(valret);
     return valret;
   };
 
-  // Used by view to build urls, resolved from url
+  // Used by view to build urls, resolved from routing
   $scope.currentAreaName  = currentAreaName;
   $scope.currentIntention = currentIntention;
   // Recipient currently resolved from url
   $scope.currentRecipient = currentRecipient;
   $scope.recipientId      = $scope.currentRecipient ? $scope.currentRecipient.Id : 'none';
   $scope.currentRecipientLabel = $scope.currentRecipient ?  $scope.currentRecipient.LocalLabel :  "";
-
-  // Give visibility to services
+  // Services visible from the view
   $scope.theHelperSvc = helperSvc;
   $scope.theQuestionBarSvc = questionBarSvc;
   $scope.filtersWellDefined = filtersSvc.wellDefined;
@@ -37,20 +38,15 @@ function ($scope, currentTextList, currentAreaName, currentIntention,  currentUs
     $scope.theAccordionStatus.open = true;
   };
 
-  // Update filters with current recipient type
-  if ( currentRecipient )
-    filtersSvc.setRecipientTypeTag(currentRecipient.RecipientTypeTag);
-
   // Unfiltered text list : initialize with resolved text list from routing
   var unfilteredTextList  = currentTextList;
-
   // Filtered text list that is actualy displayed
   $scope.filteredList = [];
 
-  // Prepare the filtered text list to be displayed.
+  // Prepare the filtered text list that is displayed.
   var firstWatchCall = true;
   var filterList = function (textListToFilter) {
-    // filterList is called at least two times when view initializes. We skip the first time :-o
+    // Optimization : filterList is called at least two times when view initializes. We skip the first time :-o
     if ( !firstWatchCall )
       filteredTextListSvc.setFilteredAndOrderedList(textListToFilter, currentUser, filtersSvc.filters.preferredStyles);
     firstWatchCall = false;
@@ -65,6 +61,9 @@ function ($scope, currentTextList, currentAreaName, currentIntention,  currentUs
     );
   };
 
+  // Update filters with current recipient type. Will be watched along with the filters
+  if ( currentRecipient )
+    filtersSvc.setRecipientTypeTag(currentRecipient.RecipientTypeTag);
   // Get and filter list when language changes
   $scope.$watch(function() { return currentLanguage.getLanguageCode(); },getAndFilterTextList( currentLanguage.currentCulture() ),true);
   // Filter list when user gender changes
