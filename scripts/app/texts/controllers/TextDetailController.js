fc16d28f1377bbfeb60bc0211f3a8931964efc13
OlivierBourdin <olivier.bourdin@gmail.com>
2014-10-14 12:43:38 +0100

    refac(TextDetailController): cosmetic changes

--- scripts/app/texts/controllers/TextDetailController.js
+++ scripts/app/texts/controllers/TextDetailController.js
@@ -1,39 +1,31 @@
-angular.module('app/texts/TextDetailController', ['common/i18n', 'common/texts/alternativeTextList','common/services/facebookHelperSvc','common/services/postActionSvc'])
+angular.module('app/texts/TextDetailController',
+['common/i18n', 'common/texts/alternativeTextList','common/services/facebookHelperSvc','common/services/postActionSvc'])
 
-// Display text with author, link to the source, usage recommandations or comments
-
+// Display text with alternative versions in other languages
 .controller('TextDetailController',
-['$scope','currentText', 'currentIntention',  'tagLabelsSvc', '$modal','currentRecipient', 'favouritesSvc','currentRecipientSvc','alternativeTextsSvc','currentLanguage','helperSvc','currentAreaName','$rootScope','$location','filtersSvc','facebookHelperSvc','postActionSvc',
-function ($scope, currentText, currentIntention, tagLabelsSvc, $modal,currentRecipient, favouritesSvc,currentRecipientSvc,alternativeTextsSvc,currentLanguage,helperSvc,currentAreaName,$rootScope,$location,filtersSvc,facebookHelperSvc,postActionSvc) {
-  // TODO : when may want to explicitly set og:title from here because facebook randomly picks the intention title instead
-
-  $scope.includeSocialPluginsOnTextPages = facebookHelperSvc.includeSocialPluginsOnTextPages;
-
-  $scope.url = $location.url();
-
-  currentText.TagLabels = tagLabelsSvc.labelsFromStyleTagIds(currentText.TagIds);
-  $scope.currentAreaName = currentAreaName;
-  $scope.currentIntention = currentIntention;
-  $scope.currentText = currentText;
-
-
-  if ( !! currentIntention )
-    $rootScope.ogDescription = currentIntention.Label;
-  else
-    console.log("no current intention");
-  //$rootScope.ogTitle = currentText.Content;
-
-  $scope.Id = currentText.TextId;
-
-
+['$scope','currentText', 'currentIntention','currentRecipient', 'currentAreaName',  'tagLabelsSvc', '$modal', 'favouritesSvc','currentRecipientSvc','alternativeTextsSvc','currentLanguage','helperSvc','$rootScope','$location','filtersSvc','facebookHelperSvc','postActionSvc',
+function ($scope, currentText, currentIntention,currentRecipient, currentAreaName, tagLabelsSvc, $modal, favouritesSvc,currentRecipientSvc,alternativeTextsSvc,currentLanguage,helperSvc,$rootScope,$location,filtersSvc,facebookHelperSvc,postActionSvc) {
   // We want an Init event even if no action takes place, in case user lands here from Google or facebook
   postActionSvc.postActionInfo('Text',currentText.TextId,'TextDetail','Init');
 
+  $scope.includeSocialPluginsOnTextPages = facebookHelperSvc.includeSocialPluginsOnTextPages;
+  $scope.url = $location.url();
+  $scope.currentAreaName = currentAreaName;
+  $scope.currentIntention = currentIntention;
+  $scope.currentText = currentText;
+  $scope.Id = currentText.TextId;
+
+  currentText.TagLabels = tagLabelsSvc.labelsFromStyleTagIds(currentText.TagIds);
+
+  // When may want to explicitly set og:title from here because facebook sometime picks the intention title instead
+  //$rootScope.ogTitle = currentText.Content;
+  if ( !! currentIntention )
+    $rootScope.ogDescription = currentIntention.Label;
 
   // Copy the text Content so that if we edit it we are not editing the original "text".
   $scope.txt = {};
-  $scope.txt.Content = currentText.Content; // has to be property of a full object to avoid prototypal inheritance problems
-
+  // Content has to be property of a full object to avoid prototypal inheritance problems
+  $scope.txt.Content = currentText.Content;
 
   // TODO : move in helper
   function adaptTextContentToLanguage(text) {
@@ -47,9 +39,7 @@ function ($scope, currentText, currentIntention, tagLabelsSvc, $modal,currentRec
     return valret;
   }
   $scope.txt.Content = adaptTextContentToLanguage(currentText);
-
   $scope.recipientId = currentRecipientSvc.getIdOfRecipient(currentRecipient);
-
   $scope.authorButton = "active";
 
   $scope.isQuote = function(txt) {
@@ -58,13 +48,12 @@ function ($scope, currentText, currentIntention, tagLabelsSvc, $modal,currentRec
 
   $scope.send = function() {
     //postActionSvc.postActionInfo('Text',currentText.TextId, 'TexDetail','send');
-
     $scope.sendDialog = $modal.open({
       templateUrl: 'views/partials/sendTextForm.html',
       scope: $scope,
       controller: 'SendTextFormController',
       resolve: {
-//        currentText: function() { return $scope.currentText; }
+        //currentText: function() { return $scope.currentText; }
         currentText: function() { return $scope.txt; }
       }
     });
@@ -75,26 +64,19 @@ function ($scope, currentText, currentIntention, tagLabelsSvc, $modal,currentRec
     $scope.editText = true;
   };
 
+  // When text is quotation, insert author name after the closing quotation mark
   $scope.addAuthor = function() {
     $scope.txt.Content = helperSvc.insertAuthorInText($scope.txt.Content, currentText.Author);
     $scope.authorButton = "disabled";
   };
 
-  $scope.isFavourite = function() {
-    return favouritesSvc.isExisting(currentText);
-  };
-
-  $scope.setFavourite = function() {
-    if ( currentIntention )
-      favouritesSvc.setFavourite(currentText, currentAreaName, currentIntention, $scope.isFavourite());
-  };
-
+  // Sender gender of this text, if defined
   $scope.getSenderGenderVariationFromCurrentUser = function (text) {
     return alternativeTextsSvc.getSenderGenderVariationFromCurrentUser(text);
   };
+  // Returns a message when text alternative is written for recipient with a different gender
   $scope.getVariationWarning = function (text) {
     var recipientWarning =  alternativeTextsSvc.getRecipientGenderVariationFromOriginal(currentText.Content,text);
-
     var valret = "Ecrit par " + alternativeTextsSvc.getGenderString(text.Sender);
     if ( recipientWarning !== "" )
       valret += " " + recipientWarning;
@@ -115,7 +97,6 @@ function ($scope, currentText, currentIntention, tagLabelsSvc, $modal,currentRec
   };
 
   alternativeTextsSvc.getRealizationList(currentAreaName,currentText.PrototypeId).then(function(textList) {
-
     // Adapt text Content formating to culture
     for (var i = 0; i < textList.length; i++) {
       var t = textList[i];
@@ -133,34 +114,14 @@ function ($scope, currentText, currentIntention, tagLabelsSvc, $modal,currentRec
       console.log("No alternative realization for " + currentText.TextId);
   });
 
-
 }]);
 
-// Make sure social buttons are displayed
-//  $facebook.getLoginStatus().then(function(response) {
-//    FB.XFBML.parse(); // fb sdk must be initialised before FB can be mentionned
-//  } );
 
-//  $scope.fbShare = function () {
-//    var url = $location.absUrl();
-////    console.log(url);
-//    ezfb.ui(
-//    {
-//      method: 'feed',
-//      name: $rootScope.pageTitle1 + " " + $rootScope.pageTitle2,
-//      picture: 'http://www.commentvousdire.com/assets/TouchWordingCompressed.png',
-//      link: url,
-//      description: currentIntention.Label
-//    },function (res) {});
+//  $scope.isFavourite = function() {
+//    return favouritesSvc.isExisting(currentText);
 //  };
-//
-//  $scope.fbSend = function () {
-//    var url = $location.absUrl();
-//    ezfb.ui({
-//      method: 'send',
-//      name: $rootScope.pageTitle1 + " " + $rootScope.pageTitle2,
-//      picture: 'http://www.commentvousdire.com/assets/TouchWordingCompressed.png',
-//      link: url,
-//      description: currentIntention.Label
-//    },function (res) { console.log(res);} );
+//  $scope.setFavourite = function() {
+//    if ( currentIntention )
+//      favouritesSvc.setFavourite(currentText, currentAreaName, currentIntention, $scope.isFavourite());
 //  };
+
