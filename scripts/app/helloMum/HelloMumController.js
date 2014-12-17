e72b11d152115c09b7e6828eb97dd225802116b9
OlivierBourdin <olivier.bourdin@gmail.com>
2014-12-17 15:14:50 +0000

    helloMum refactoring

--- scripts/app/helloMum/HelloMumController.js
+++ scripts/app/helloMum/HelloMumController.js
@@ -1,39 +1,27 @@
 angular.module('app/helloMum/HelloMumController', [])
 .controller('HelloMumController', ['$scope','currentLanguage','$q','helloMumSvc',
 function($scope, currentLanguage,$q,helloMumSvc) {
+  // TODO : if we know the user gender, we should set it before calling filtering functions (or use a watch to refilter)
+  //helloMumSvc.setUserGender('H'); // you would do that if you learn that recipient gender is Male
+  // TODO : add mechanism to check for cache staleness somewhere in the app
 
-  // Get the qualified slugs of the hello mum intentions
-  var arrayOfIntentionsWithTexts = helloMumSvc.getQualifiedIntentionsSlugs();
+  // Get slugs and default weights for hello mum intentions (
+  var weightedIntentions = helloMumSvc.intentionDefaultWeights();
 
-  // TODO : adjust intention userWeight properties according to user preferences (none, few, many)
+  // TODO : adjust intention userWeight properties according to user choice (none, few, many)
   // .....
 
-  // Calculate weight of each intention according to defaultWeight and userWeight
-  arrayOfIntentionsWithTexts = helloMumSvc.calculateWeight(arrayOfIntentionsWithTexts);
+  // Set intention weights = defaultWeight * userWeight
+  helloMumSvc.setIntentionWeights(weightedIntentions);
 
-  // Get promises for each text list (from cache if previously queried)
-  // TODO : add mechanism to check for cache staleness
-  var culture = currentLanguage.currentCulture(); // could just use 'en-GB' instead
-  var arrayOfPromises = helloMumSvc.getPromisesForTextLists(arrayOfIntentionsWithTexts,culture);
+  // Get text list promises for the intentions (from cache if previously queried)
 
-  // When all promises are resolved
-  $q.all(arrayOfPromises).then(function (resolvedTextLists) {
+  currentLanguage.setLanguageCode('en',true); // Should be set when app initialize
+  var textListPromises = helloMumSvc.textListPromises(weightedIntentions,currentLanguage.currentCulture());
 
-    //helloMumSvc.setUserGender('H'); // you would do that if you learn that recipient gender is Male
-
-    var nbFilteredTexts = 0;  var nbTextsForIntentions = 0;
-    for (var i = 0; i < arrayOfIntentionsWithTexts.length; i++) {
-      // Keep texts with relationType == Parent, recipientGender == F
-      var texts = resolvedTextLists[i];
-      var filteredText = helloMumSvc.filterTextsForMother(texts);
-      arrayOfIntentionsWithTexts[i].texts = filteredText;
-
-      console.log(arrayOfIntentionsWithTexts[i].name + ' ' + texts.length + ' ' + filteredText.length);
-      nbFilteredTexts += filteredText.length;   nbTextsForIntentions += texts.length;
-    }
-    console.log("------ TOTAL : " + nbTextsForIntentions);  console.log("------ TOTAL FILTERED : " + nbFilteredTexts);
-
-    // TODO : also filter for sender gender and closeness
+  // Filter and pick texts
+  $q.all(textListPromises).then(function (resolvedTextLists) {
+    helloMumSvc.attachFilteredTextListsToWeightedIntentions(weightedIntentions,resolvedTextLists);
 
     // If we suppose that 'burnt' texts = texts previously disliked + texts previously sent + text previously ignored
     // Before we make a random choice, we could check that the number of burnt text
@@ -41,8 +29,9 @@ function($scope, currentLanguage,$q,helloMumSvc) {
     // if so we could recycle (unBurn) those that have been previously ignored
     //var chosenText = chooseTextFromArrayOfTextList(chosenTexts);
 
-    var chosenTexts = helloMumSvc.pickOneArrayOfTextArrays(arrayOfIntentionsWithTexts).texts;
-    var chosenText = helloMumSvc.pickOneTextInArray(chosenTexts);
+//    var chosenTexts = helloMumSvc.pickOneWeightedIntention(weightedIntentions).texts;
+//    var chosenText = helloMumSvc.pickOneTextFromTextList(chosenTexts);
+    var chosenText = helloMumSvc.pickOneTextFromWeightedIntentionArray(weightedIntentions);
     console.log(chosenText);
 
     // TODO : if chosen text is burnt, require another one instead
--- scripts/app/helloMum/helloMumSvc.js
+++ scripts/app/helloMum/helloMumSvc.js
@@ -5,9 +5,9 @@ function (randomHelperSvc,textsSvc,filterHelperSvc,currentUser) {
 
   var service = {
 
-    // intention defaultWeight : will be 1 by default, between 0 and 1 if we feel that an intention might contain too many text
-    // intention userWeight : will be 0, 1 or 4 if users chooses none, few or many for this intention
-    getQualifiedIntentionsSlugs: function () {
+    // defaultWeight : 1 by default, between 0 and 1 if we feel an intention contains too many texts
+    // userWeight    : (0, 1 or 4) <=> (none, few,  many)
+    intentionDefaultWeights: function () {
       var arr = [
         { name: 'how-are-you',        defaultWeight: 1,   userWeight: 1 },
         { name: 'I-think-of-you',     defaultWeight: 1,   userWeight: 1 },
@@ -18,44 +18,23 @@ function (randomHelperSvc,textsSvc,filterHelperSvc,currentUser) {
         { name: 'I-miss-you',         defaultWeight: 1,   userWeight: 1 },
         { name: 'I-am-here-for-you',  defaultWeight: 0.5, userWeight: 1 },
         { name: 'facebook-status',    defaultWeight: 0.5, userWeight: 1 },
-        { name: 'positive-thoughts',   defaultWeight: 1,  userWeight: 1 },
+        { name: 'positive-thoughts',  defaultWeight: 1,  userWeight: 1 },
         { name: 'would-you-care-for-a-drink', defaultWeight: 1,   userWeight: 1 },
       ];
     return arr;
     },
 
-    calculateWeight: function(arr) {
+    // add a weight field.
+    setIntentionWeights: function(arr) {
       for (var i = 0; i < arr.length; i++) {
         arr[i].weight = arr[i].defaultWeight * arr[i].userWeight;
       }
-      return arr;
     },
 
-    getPromisesForTextLists: function(arr,culture) {
-      var textListPromises = [];
-      for (var i=0; i< arr.length; i++) {
-        // TODO : when we have the API for that, we can add relation type to the parameters
-        var p = textsSvc.getTextList('HelloMum',arr[i].name,culture,true,false);
-        textListPromises.push(p);
-      }
-      return textListPromises;
+    setUserGender: function(gender) {
+      if ( gender == 'F' || gender == 'H' ) // H for Homme for Male
+        currentUser.gender = gender;
     },
-
-    pickOneArrayOfTextArrays: function (arr) {
-      // Add size information to each text list
-      for (var i = 0; i < arr.length; i++) {
-        arr[i].size = arr[i].texts.length;
-        //console.log(arr[i].name + ' ' + arr[i].size);
-      }
-      // Choose one of the array
-      var chosenArray = randomHelperSvc.weightedRandom(arr);
-      return chosenArray;
-    },
-
-    pickOneTextInArray:function(textArray) {
-      return randomHelperSvc.weightedRandom(textArray);
-    },
-
     createMotherFilter: function() {
       var filters = filterHelperSvc.createEmptyFilters();
       // Set relation type to parent
@@ -67,13 +46,49 @@ function (randomHelperSvc,textsSvc,filterHelperSvc,currentUser) {
     filterTextsForMother: function(texts) {
       var filters = service.createMotherFilter();
       // currentUser will have all properties to default unless you set one.
-      var filteredTexts = filterHelperSvc.getFilteredList(texts, currentUser, filters);
-      return filteredTexts;
+      return  filterHelperSvc.getFilteredList(texts, currentUser, filters);
     },
-    setUserGender: function(gender) {
-      if ( gender == 'F' || gender == 'H' ) // H for Homme for Male
-        currentUser.gender = gender;
+
+    textListPromises: function(arr,culture) {
+      var textListPromises = [];
+      for (var i=0; i< arr.length; i++) {
+        // TODO : when we have the API for that, we can add relation type to the parameters
+        var p = textsSvc.getTextList('HelloMum',arr[i].name,culture,true,false);
+        textListPromises.push(p);
+      }
+      return textListPromises;
+    },
+
+    attachFilteredTextListsToWeightedIntentions: function (weightedIntentions,resolvedTextLists) {
+      var nbFilteredTexts = 0;  var nbTextsForIntentions = 0;
+      for (var i = 0; i < weightedIntentions.length; i++) {
+        var texts = resolvedTextLists[i];
+        var filteredText = service.filterTextsForMother(texts);
+        console.log(weightedIntentions[i].name + ' ' + texts.length + ' ' + filteredText.length);
+
+        weightedIntentions[i].texts = filteredText;
+        nbFilteredTexts += filteredText.length;   nbTextsForIntentions += texts.length;
+      }
+      console.log("------ TOTAL : " + nbTextsForIntentions);  console.log("------ TOTAL FILTERED : " + nbFilteredTexts);
+    },
+
+    pickOneWeightedIntention: function (weightedIntentions) {
+      // We could set sizes to one if we don't want the number of elements of influence chances of being picked
+      for (var i = 0; i < weightedIntentions.length; i++) {
+        weightedIntentions[i].size = weightedIntentions[i].texts.length;
+      }
+      // Choose one of the text lists
+      return randomHelperSvc.weightedRandom(weightedIntentions);
+    },
+    pickOneTextFromTextList:function(textList) {
+      // Each text will have size==1 and weight == 1 by default
+      return randomHelperSvc.weightedRandom(textList);
+    },
+    pickOneTextFromWeightedIntentionArray: function (weightedIntentions) {
+      var chosenTexts = service.pickOneWeightedIntention(weightedIntentions).texts;
+      var chosenText  = service.pickOneTextFromTextList(chosenTexts);
     }
+
 
 };
 
