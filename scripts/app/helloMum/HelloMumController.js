cf6f3a8ad96baaeed4857df0ad6b8328e72f384d
OlivierBourdin <olivier.bourdin@gmail.com>
2015-03-21 13:47:17 +0000

    refac(HelloMumController) : tidy up the welcome text group fetching

--- scripts/app/helloMum/HelloMumController.js
+++ scripts/app/helloMum/HelloMumController.js
@@ -1,57 +1,38 @@
 angular.module('app/helloMum/HelloMumController', [])
+
 .controller('HelloMumController', ['$scope','currentLanguage','$q','helloMumTextsSvc','helperSvc','intentionPonderationSvc','weightedTextRandomPickSvc','textsSvc','welcomeGroupTextSelectionSvc',
 function($scope, currentLanguage,$q,helloMumTextsSvc,helperSvc,intentionPonderationSvc,weightedTextRandomPickSvc,textsSvc,welcomeGroupTextSelectionSvc) {
 
-
-
-  var getMumWelcomeTextList =  function (areaName, culture, groupId) {
-
-    // Find welcome group depending of language code of culture
-    function findGroupIdForCulture(culture) {
-      var groupId = '53A0E1';
-      var languageCode = 'en';
-      if (culture.length >= 2)
-        languageCode = culture.substring(0, 2);
-
-      switch (languageCode) {
-        case 'fr':
-          groupId = '774EE7';
-          break;
-        default:
-          groupId = '53A0E1';
-          break;
-      }
-      return groupId;
+  // WELCOME TEXT GROUP
+  // Get welcome text groups for mums : a list of texts that will be displayed when the app launches for the first time
+  var getMumWelcomeTextList =  function () {
+    var groupId;
+    switch (currentLanguage.getLanguageCode()) {
+      case 'fr':
+        groupId = '774EE7';
+        break;
+      default:
+        groupId = '53A0E1';
+        break;
     }
-
-    if (!groupId) {
-      groupId = findGroupIdForCulture(culture);
-    }
-
-    var texts = textsSvc.getTextListForGroup(areaName, groupId, culture, true, true);
-    return texts;
+    return textsSvc.getTextListForGroup('HelloMum', groupId, currentLanguage.getCultureCode(), true, true);
   };
 
+  // For mums, we do not want the first texts to be is Jokes, facebook status, positive thoughts, or other impersonal texts
+  var excludeTextFromFirstPositionOfMumTextList = function(text) {
+    var intentionId = text.IntentionId;
+    if (  intentionId == "0B1EA1" || intentionId == "2E2986" || intentionId == "A64962" || intentionId == "67CC40" )
+      return true;
+    return false;
+  };
 
-  getMumWelcomeTextList('HelloMum','en-EN','53A0E1').then(function(texts) {
-    // does not work :
-//    $scope.welcomeTexts = helloMumTextsSvc.filterTextsForHelloMum(texts);
-
-    //var selectedTexts = helloMumTextsSvc.pickWelcomeTexts(texts,8);
-    var selectedTexts = welcomeGroupTextSelectionSvc.pickWelcomeTexts(texts,8);
-
-
-
-//    $scope.welcomeTexts = texts;
-    $scope.welcomeTexts = selectedTexts;
-
-//    for (var i =0; i< texts.length; i++)
-//      console.log(texts[i]);
+  // Get the welcome text list then pick a few for display
+  getMumWelcomeTextList().then(function(texts) {
+    $scope.welcomeTexts = welcomeGroupTextSelectionSvc.pickWelcomeTexts(texts,8,excludeTextFromFirstPositionOfMumTextList);
   });
 
-  // TODO : if we know the user gender, we should set it before calling filtering functions (or use a watch to refilter)
-  //helloMumSvc.setUserGender('H'); // you would do that if you learn that recipient gender is Male
-  // TODO : add mechanism to check for cache staleness somewhere in the app
+
+  // FETCH A TEXT LIST FOR EACH INTERESTING INTENTION
 
   // Get slugs and default weights for hello mum intentions (
   var weightedIntentions = intentionPonderationSvc.intentionDefaultWeights();
@@ -70,19 +51,13 @@ function($scope, currentLanguage,$q,helloMumTextsSvc,helperSvc,intentionPonderat
   // Set intention weights = defaultWeight * userWeight
   intentionPonderationSvc.setIntentionWeights(weightedIntentions);
 
-  // Get text list promises for the intentions (from cache if previously queried)
-
-  currentLanguage.setLanguageCode('en',false); // Should be set when app initialize, or use 'en-EN'
+  // Get text list promises for each intentions (from cache if previously queried)
   var textListPromises = helloMumTextsSvc.textListPromises(weightedIntentions,currentLanguage.currentCulture()); // 'en-EN' can be used as hard coded culture
 
-    $scope.choices = [];
-
-  // When all texts have been fetched
+  $scope.choices = [];
+  // When text lists have been fetched for all intentions, attach them to the weigthed intentions so they can be picked
   $q.all(textListPromises).then(function (resolvedTextLists) {
 
-    //var filteredTextLists = helloMumTextsSvc.filterTextsForHelloMum(resolvedTextLists);
-
-    // Associate texts lists with weighted intentions
     helloMumTextsSvc.attachFilteredTextListsToWeightedIntentions(weightedIntentions,resolvedTextLists);
     // Pick 8 texts
     for (var i = 0; i < 8;  i++  ) {
@@ -94,34 +69,26 @@ function($scope, currentLanguage,$q,helloMumTextsSvc,helperSvc,intentionPonderat
     }
   });
 
-  // Will try to pick a random text until satisfied
+
+  // Try to pick a random text until satisfied
   var chooseText = function(weightedIntentions) {
     var choice;
     var happyWithChoice = true;
     do {
        choice = weightedTextRandomPickSvc.pickOneTextFromWeightedIntentionArray(weightedIntentions);
     } while (happyWithText(choice.text) === false);
-    //console.log(choice.text.SortBy + ' ** ' + choice.text.Content);
-    //console.log(choice.text);
     return choice;
   };
-
-  // Let us say that 'burnt' texts are texts disliked + texts already sent
-  // We don't want them
-
-  // TODO : if chosen text is burnt, express deep sadness
-  var happyWithText = function(pickedText) {
-    // TODO : check that text has not been picked during this session
-    // if matching text Id in $scope.choices, return false
-    // TODO : check that text has not been disliked previously
-    // TODO : check that text has not already been sent
-    return true;
-  };
+  // Always returns true, instead we could check that a text has not been previously disliked, or seen to many times
+  var happyWithText = function(pickedText) { return true; };
 
   $scope.isQuote = function(txt) {
-    return helperSvc.isQuote(txt); };
+    return helperSvc.isQuote(txt);
+  };
 
-  // TODO : we also want to check that the number of burnt text is not too large
-  // as compared to the number of available texts (for example nbBurnt < available * 0.80)
+  // TODO for real apps : if we know the user gender, we should set it before calling filtering functions
+  //helloMumSvc.setUserGender('H'); // you would do that if you learn that recipient gender is Male
+  // TODO for real apps  : add mechanism to check for cache staleness
+
 }]);
 
