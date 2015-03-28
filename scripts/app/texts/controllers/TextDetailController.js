4b1eeb653f4a30f638df7833c81f42058e0327f7
OlivierBourdin <olivier.bourdin@gmail.com>
2015-03-28 23:01:34 +0000

    feat(textDetailController) : display random image

--- scripts/app/texts/controllers/TextDetailController.js
+++ scripts/app/texts/controllers/TextDetailController.js
@@ -7,9 +7,9 @@ angular.module('app/texts/TextDetailController',[
 // Display text with alternative versions in other languages
 .controller('TextDetailController',
 ['$scope','currentText', 'currentAreaName', 'currentIntentionSlugOrId','currentIntentionLabel','currentRecipientId',
-          'tagLabelsSvc',  'alternativeTextsSvc','currentLanguage','helperSvc','$rootScope','$location','filtersSvc','facebookHelperSvc','postActionSvc','$modal',
+          'tagLabelsSvc',  'alternativeTextsSvc','currentLanguage','helperSvc','$rootScope','$location','filtersSvc','facebookHelperSvc','postActionSvc','$modal','serverSvc','$http',
 function ($scope, currentText,  currentAreaName, currentIntentionSlugOrId,currentIntentionLabel, currentRecipientId,// those variables are resolved in routing.js
-          tagLabelsSvc, alternativeTextsSvc,currentLanguage,helperSvc,$rootScope,$location,filtersSvc,facebookHelperSvc,postActionSvc, $modal) {
+          tagLabelsSvc, alternativeTextsSvc,currentLanguage,helperSvc,$rootScope,$location,filtersSvc,facebookHelperSvc,postActionSvc, $modal,serverSvc,$http) {
 
   // We want an Init event even if no action takes place, in case user lands here from Google or facebook
   postActionSvc.postActionInfo('Text',currentText.TextId,'TextDetail','Init');
@@ -62,6 +62,52 @@ function ($scope, currentText,  currentAreaName, currentIntentionSlugOrId,curren
   $scope.isVariationFormMorePrecise = function(text) {
     return alternativeTextsSvc.isVariationFormMorePrecise(currentText,text);
   };
+
+
+  var getStatic = function(path, params, skipTracker, optionalCulture) {
+    var culture = optionalCulture !== undefined ? optionalCulture : currentLanguage.currentCulture();
+    return $http({
+      method: 'GET',
+      url : "http://" + path,
+      headers: {
+        'Accept-Language': culture,
+        'Ghost-Language': culture,
+        'Content-Type': 'application/json'
+      },
+      cache: false,
+      params: params,
+      // Not all http get activate the tracker (reporting users actions and pre-fetching data should not activate a spinner)
+      tracker: skipTracker !== true ? $rootScope.loadingTracker : null
+    }).then(
+    function (response) {
+      console.log('Request for "' + path + '" responded with ' + response.status + ' for culture ' + culture);
+      return response.data;
+    },
+    function (error) {
+      console.log("error");
+      //return $q.reject(path + 'rejected in serverSvc : ');
+    }
+
+    );
+  };
+
+  var getImageForText = function() {
+    var retval = "http://gw-static.azurewebsites.net/cvd/sweetheart/stocklove/small/10624691_581498388628102_7259835679150402181_n.jpg";
+
+    var requete = "gw-static.azurewebsites.net/container/randomfile/cvd?size=small";
+    //var requete = "gw-static.azurewebsites.net/container/randomfile/cvd";
+//    serverSvc.get(requete, null).then( function (chemin) {
+//      console.log(chemin);
+//    });
+    return getStatic(requete, undefined,true).then(function(response) {
+      //$scope.imagePath = retval;
+      console.log(response);
+      $scope.imagePath = "http://gw-static.azurewebsites.net" + response;
+    });
+    //return retval;
+  };
+
+  getImageForText();
 
   // For each orderedPresentationLanguages, prepare an array of available texts for the language, then chose the best ones according to sender, recipient and polite form
   alternativeTextsSvc.getRealizationList(currentAreaName,currentText.PrototypeId).then(function(textList) {
