ec7b8353899fed27f806101eaead4d505ebe5241
rui <github@rui.fr>
2020-11-09 12:46:45 +0100

    rc: updates to todays versions and fix breaking changes (gulp+express)

--- server.js
+++ server.js
@@ -3,35 +3,33 @@ var app = express();
 var util = require('util');
 var colors = require('colors');
 
-app.configure(function() {
-  app.use(express.static(__dirname));
-});
+app.use(express.static(__dirname));
 
 app.get('/assets/*',function(req, res, next) {
-  util.puts('file: ' + req.url);
-  res.sendfile(__dirname + req.url);
+  util.debuglog('file: ' + req.url);
+  res.sendFile(__dirname + req.url);
 });
 
 app.get('/scripts/*',function(req, res, next) {
-  util.puts('file: ' + req.url);
-  res.sendfile(__dirname + req.url);
+  util.debuglog('file: ' + req.url);
+  res.sendFile(__dirname + req.url);
 });
 
 app.get('/views/*',function(req, res, next) {
-  util.puts('file: ' + req.url);
-  res.sendfile(__dirname  + req.url);
+  util.debuglog('file: ' + req.url);
+  res.sendFile(__dirname  + req.url);
 });
 
 app.get('/bower_components/*',function(req, res, next) {
-  util.puts('file: ' + req.url);
-  res.sendfile(__dirname  + req.url);
+  util.debuglog('file: ' + req.url);
+  res.sendFile(__dirname  + req.url);
 });
 
 app.get("/*", function(req, res, next) {
-  util.puts('path: ' + req.url);
-  res.sendfile(__dirname + '/index.html');
+  util.debuglog('path: ' + req.url);
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000);
 
-util.puts('server started on port 3000'.blue);
+util.debuglog('server started on port 3000'.blue);
